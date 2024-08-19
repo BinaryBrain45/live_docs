d6b32155bd5b4bee4f349ef8e0d437b50f16f1c1
Jaya Prakash <167225513+BinaryBrain45@users.noreply.github.com>
2024-08-19 14:55:49 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
 org: "vjit-zm",
-project: "javascript-nextjs",
+project: "live-docs",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
